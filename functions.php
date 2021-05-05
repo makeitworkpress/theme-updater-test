c30f8a542bf82cf4b07a894a73c4d2f55d73d319
Michiel Tramper <michielsite@hotmail.com>
2021-05-05 17:06:05 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,21 +2,33 @@
 /**
  * Set-up our autoloader from composer
  */
-spl_autoload_register( function($classname) {
-
-    $class      = str_replace( '\\', DIRECTORY_SEPARATOR, str_replace( '_', '-', strtolower($classname) ) );
-    $vendor     = str_replace( 'makeitworkpress' . DIRECTORY_SEPARATOR, '', $class );
-    $vendor     = 'makeitworkpress' . DIRECTORY_SEPARATOR . preg_replace( '/\//', '/src/', $vendor, 1 ); // Replace the first slash for the src folder
-    $vendors    = dirname(__FILE__) .  DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . $vendor . '.php';
+spl_autoload_register( function($className) {
     
-    if ( file_exists( $vendors) ) {
-        include_once $vendors;
+    $calledClass    = str_replace( '\\', DIRECTORY_SEPARATOR, str_replace( '_', '-', strtolower($className) ) );
+    $parentDir      = get_template_directory() . DIRECTORY_SEPARATOR;
+    
+    // Require main parent classes
+    $parentClass    = $parentDir . 'classes' . DIRECTORY_SEPARATOR . $calledClass . '.php';
+
+    if( file_exists($parentClass) ) {
+        require_once( $parentClass );
+        return;
+    } 
+    
+    // Require Vendor (composer) classes
+    $classNames     = explode(DIRECTORY_SEPARATOR, $calledClass);
+    array_splice($classNames, 2, 0, 'src');
+
+    $vendorClass    = $parentDir . 'vendor' . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $classNames) . '.php';
+
+    if( file_exists($vendorClass) ) {
+        require_once( $vendorClass );    
     }
-
+   
 } );
-
 
 /**
  * Set-up the updater
  */
-new MakeitWorkPress\WP_Updater\Boot(['type' => 'theme', 'source' => 'https://github.com/makeitworkpress/theme-updater-test']);
+$updater = MakeitWorkPress\WP_Updater\Boot::instance();
+$updater->add(['type' => 'theme', 'source' => 'https://github.com/makeitworkpress/theme-updater-test']);
